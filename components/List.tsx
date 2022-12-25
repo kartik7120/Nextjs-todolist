7bf09aac5ec63d692512808b615968b9f8aedf51
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-25 19:04:25 +0530

    Added a model for deleting a list

--- components/List.tsx
+++ components/List.tsx
@@ -1,7 +1,9 @@
-import { Button, Card, Tooltip } from "@nextui-org/react";
+import { Button, Card, Modal, Text, Tooltip } from "@nextui-org/react";
+import { Model } from "mongoose";
 import { useRouter } from "next/router";
 import { BsTrash } from "react-icons/bs";
 import { useSWRConfig } from "swr";
+import React from "react";
 
 interface Props {
     header: string | undefined,
@@ -12,6 +14,14 @@ interface Props {
 export default function ListCard(props: Props) {
     const router = useRouter();
     const { mutate } = useSWRConfig();
+
+    const [visible, setVisible] = React.useState(false);
+    const handler = () => setVisible(true);
+
+    const closeHandler = () => {
+        setVisible(false);
+    };
+
 
     async function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
         try {
@@ -34,22 +44,45 @@ export default function ListCard(props: Props) {
         }
     }
 
-    return <Card css={{ mw: "300px", margin: "$5" }} variant='bordered' isHoverable isPressable onPress={
-        () => {
-            router.push(`/${props.id}`)
-        }
-    }>
-        <Card.Header>
-            {props.header}
-        </Card.Header>
-        <Card.Divider />
-        <Card.Body>
-            {props.body}
-        </Card.Body>
-        <Card.Footer>
-            <Tooltip content="Delete List">
-                <Button type="button" color="error" size="md" icon={<BsTrash />} onClick={handleClick} />
-            </Tooltip>
-        </Card.Footer>
-    </Card>
+    return <>
+        <Modal
+            closeButton
+            aria-labelledby="modal-title"
+            open={visible}
+            onClose={closeHandler}
+        >
+            <Modal.Header>
+                <Text size="$lg">
+                    Are you sure ?
+                </Text>
+            </Modal.Header>
+            <Modal.Body>
+                <Button type="button" color="default" size="md" onClick={closeHandler}>
+                    No
+                </Button>
+                <Button type="button" color="primary" size="md" onClick={handleClick}>
+                    Yes
+                </Button>
+            </Modal.Body>
+        </Modal>
+
+        <Card css={{ mw: "300px", margin: "$5" }} variant='bordered' isHoverable isPressable onPress={
+            () => {
+                router.push(`/${props.id}`)
+            }
+        }>
+            <Card.Header>
+                {props.header}
+            </Card.Header>
+            <Card.Divider />
+            <Card.Body>
+                {props.body}
+            </Card.Body>
+            <Card.Footer>
+                <Tooltip content="Delete List">
+                    <Button type="button" color="error" size="md" icon={<BsTrash />} onClick={handler} />
+                </Tooltip>
+            </Card.Footer>
+        </Card>
+    </>
 }
